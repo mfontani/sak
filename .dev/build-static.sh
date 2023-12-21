302612d19b887f9d3f91cdd7ffc9c476b32434e3
Marco Fontani <MFONTANI@cpan.org>
2023-12-21 10:46:21 +0100

    use CGO_ENABLED=0 to better statically compile
    ... as v0.0.12 didn't work on my machines.

--- .dev/build-static.sh
+++ .dev/build-static.sh
@@ -2,4 +2,4 @@
 
 # fatal: detected dubious ownership in repository at '...'
 git config --global --add safe.directory "$(pwd)"
-go build -tags timetzdata --ldflags "-X 'main.Version=$(git describe --tags)' -extldflags \"-static\" -s -w" -o sak .
+CGO_ENABLED=0 go build -tags timetzdata --ldflags "-X 'main.Version=$(git describe --tags)' -extldflags \"-static\" -s -w" -o sak .
